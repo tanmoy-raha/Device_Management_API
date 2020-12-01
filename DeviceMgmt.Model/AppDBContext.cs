fbd6fecddc0844fc5cca9ffb797363056bce7b27
Tanmoy Raha <tanmoy.raha@relate-infotech.com>
2020-12-02 01:31:00 +0530

    Add project files.

---
+++ DeviceMgmt.Model/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceMgmt.API.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=ClientDb;Trusted_Connection=True;";
+
+        public ApplicationDBContext() : base() { }
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+        //public DbSet<Person> People { get; set; }
+
+       
+    }
+}
---
+++ DeviceMgmt.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DeviceMgmt.Service
+{
+    public class Class1
+    {
+    }
+}
